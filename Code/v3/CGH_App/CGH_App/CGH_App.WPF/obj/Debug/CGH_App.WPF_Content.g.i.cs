50383049e519457131c8a59bf5a83baff6908fb6
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-06-01 22:52:23 +1200

    add new image

--- Code/v3/CGH_App/CGH_App/CGH_App.WPF/obj/Debug/CGH_App.WPF_Content.g.i.cs
+++ Code/v3/CGH_App/CGH_App/CGH_App.WPF/obj/Debug/CGH_App.WPF_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("image/betting.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("image/grandstand.png")]
 
 
