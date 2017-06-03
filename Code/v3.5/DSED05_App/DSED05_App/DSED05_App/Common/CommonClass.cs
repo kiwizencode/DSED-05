a62960f51d5801d4f9df1c63b581018a53a65984
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-06-03 18:10:43 +1200

    create 2 Class CommonClass and a Singleton Class

---
+++ Code/v3.5/DSED05_App/DSED05_App/DSED05_App/Common/CommonClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSED05_App.Common
+{
+    public static class CommonClass
+    {
+        /// <summary>
+        /// Define the "Game" Parameter Type 
+        /// </summary>
+        public enum Game_Parameter_Type { Size, Speed, Punter_Type };
+    }
+}
---
+++ Code/v3.5/DSED05_App/DSED05_App/DSED05_App/Common/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSED05_App.Common
+{
+    /// <summary>
+    /// Singleton Design Pattern
+    /// </summary>
+    public sealed class RandomGeneratorSingleton
+    {
+        /// <summary>
+        /// using .NET 4's Lazy<T> type
+        /// </summary>
+        private static readonly Lazy<RandomGeneratorSingleton> lazy = new Lazy<RandomGeneratorSingleton>(() => new RandomGeneratorSingleton());
+        public static RandomGeneratorSingleton Instance { get { return lazy.Value; } }
+
+        private Random _random;
+        public Random Seed { get { return _random; } }
+        private RandomGeneratorSingleton()
+        {
+            _random = new Random(System.DateTime.Now.Millisecond);
+        }
+    }
+}
