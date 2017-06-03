2050642830de23dbabb927105987c35d42dbc8f7
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-06-03 18:54:27 +1200

    save code

---
+++ Code/v3.5/RacerApp/RacerApp/RacerApp/Common/CommonClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RacerApp.Common
+{
+    public static class CommonClass
+    {
+        /// <summary>
+        /// Define the "Game" Parameter Type 
+        /// </summary>
+        public enum Game_Parameter_Type { Size, Speed, Punter_Type };
+
+    }
+}
---
+++ Code/v3.5/RacerApp/RacerApp/RacerApp/Common/Singleton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RacerApp.Common
+{
+    /// <summary>
+    /// Singleton Design Pattern
+    /// </summary>
+    public sealed class RandomGenerator
+    {
+        /// <summary>
+        /// using .NET 4's Lazy<T> type
+        /// </summary>
+        private static readonly Lazy<RandomGenerator> lazy = new Lazy<RandomGenerator>(() => new RandomGenerator());
+        public static RandomGenerator Instance { get { return lazy.Value; } }
+
+        private Random _random;
+        public Random Seed { get { return _random; } }
+        private RandomGenerator()
+        {
+            _random = new Random(System.DateTime.Now.Millisecond);
+        }
+
+        /// <summary>
+        /// Generate a random sequence of enum type specified by the input parameter
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public IEnumerator<Enum> GetRandomSequence(CommonClass.Game_Parameter_Type type)
+        {
+            switch (type)
+            {
+                case CommonClass.Game_Parameter_Type.Size:
+                    return Enum.GetValues(typeof(CommonClass.Size)).Cast<Enum>().OrderBy(x => _random.Next()).ToArray();
+                case CommonClass.Game_Parameter_Type.Speed:
+                    return Enum.GetValues(typeof(CommonClass.Speed)).Cast<Enum>().OrderBy(x => _random.Next()).ToArray();
+                case CommonClass.Game_Parameter_Type.Punter:
+                    return Enum.GetValues(typeof(CommonClass.Punter_Type)).Cast<Enum>().OrderBy(x => _random.Next()).ToArray();
+                default: throw new NotImplementedException();
+            }
+        }
+    }
+}
