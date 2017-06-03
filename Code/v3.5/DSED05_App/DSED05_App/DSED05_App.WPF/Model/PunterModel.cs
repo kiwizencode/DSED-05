eaff5d460dad38883ceaab8429870d4517a78de8
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-06-03 22:33:15 +1200

    ammend code

--- Code/v3.5/DSED05_App/DSED05_App/DSED05_App.WPF/Model/PunterModel.cs
+++ Code/v3.5/DSED05_App/DSED05_App/DSED05_App.WPF/Model/PunterModel.cs
@@ -13,14 +13,57 @@ namespace DSED05_App.WPF.Model
     {
         public Image Image { get; private set;}
 
-        public string Name { get => _Punter.Name; set { _Punter.Name = value; } }
-        public int Money { get => _Punter.Money; }
-      
         private AbstractPunter _Punter;
 
-        public PunterModel(CommonClass.Punter_Type type)
+        public string Name { get => _Punter.Name; set { _Punter.Name = value; } }
+        public int Money { get => _Punter.Money; private set { _Punter.Money = value; } }
+
+        public const int NOT_BETTING = 0;
+        public const int NO_RACER_SELECTED = -1;
+
+        /// <summary>
+        /// The ID of the racer that punter has placed bet on
+        /// </summary>
+        public int RacerID = NO_RACER_SELECTED;     
+
+        /// <summary>
+        /// How much money the punter has place on a particular dog
+        /// </summary>
+        public int Bet = NOT_BETTING;
+
+        public PunterModel(CommonClass.Punter_Type type, Image image)
         {
             _Punter = PunterGenerator.FactoryMethod(type);
+            Image = image;
+        }
+
+        /// <summary>
+        /// check whether the punter has no more money left ==> BUSTED !!
+        /// </summary>
+        public bool Busted { get => (Money > 0 ? true : false); }
+
+        /// <summary>
+        /// When punter win a game, add bet amount to Money and reset bet amount.
+        /// </summary>
+        public void WinGame()
+        {
+            Money += Bet;
+            ResetBet();
+        }
+        /// <summary>
+        /// When punter win a game, add bet amount to Money and reset bet amount.
+        /// </summary>
+        public void LoseGame()
+        {
+            Money -= Bet;
+            ResetBet();
+        }
+        /// <summary>
+        /// Reset bet amount.
+        /// </summary>
+        public void ResetBet()
+        {
+            Bet = NOT_BETTING;
         }
     }
 }
--- Code/v3.5/DSED05_App/DSED05_App/DSED05_App/Data/Punter/AbstractPunter.cs
+++ Code/v3.5/DSED05_App/DSED05_App/DSED05_App/Data/Punter/AbstractPunter.cs
@@ -7,9 +7,6 @@ namespace DSED05_App.Data.Punter
 {
     public abstract class AbstractPunter : IData
     {
-        public const int NOT_BETTING = 0;
-        public const int NO_RACER_SELECTED = -1;
-
         public AbstractPunter()
         {
             ClassName = "Abstract Punter Class";
@@ -23,45 +20,10 @@ namespace DSED05_App.Data.Punter
         /// Money that the punter have
         /// </summary>
         public int Money { get; set; }
-        /// <summary>
-        /// check whether the punter has no more money left ==> BUSTED !!
-        /// </summary>
-        public bool Busted { get => (Money > 0 ? true : false); }
-        /// <summary>
-        /// How much money the punter has place on a particular dog
-        /// </summary>
-        public int Bet = NOT_BETTING;
-        /// <summary>
-        /// The ID of the dog that punter has placed bet on
-        /// </summary>
-        public int RacerID = NO_RACER_SELECTED;
 
         public bool CheckType(Enum type)
         {
             return (CommonClass.Punter_Type)Type == (CommonClass.Punter_Type)type;
-        }
-        /// <summary>
-        /// When punter win a game, add bet amount to Money and reset bet amount.
-        /// </summary>
-        public void WinGame()
-        {
-            Money += Bet;
-            ResetBet();
-        }
-        /// <summary>
-        /// When punter win a game, add bet amount to Money and reset bet amount.
-        /// </summary>
-        public void LoseGame()
-        {
-            Money -= Bet;
-            ResetBet();
-        }
-        /// <summary>
-        /// Reset bet amount.
-        /// </summary>
-        public void ResetBet()
-        {
-            Bet = NOT_BETTING;
         }
     }
 }
